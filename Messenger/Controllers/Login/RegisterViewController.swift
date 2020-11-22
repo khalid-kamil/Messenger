cab76f03a0a4483ed2c23e9c429c3c6524b22a39
Khalid <khalidkamil@icloud.com>
2020-11-22 18:24:15 +0000

    Set Up Firebase and Email/Password Register and Log In

--- Messenger/Controllers/Login/RegisterViewController.swift
+++ Messenger/Controllers/Login/RegisterViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
     
@@ -168,7 +169,16 @@ class RegisterViewController: UIViewController {
             return
         }
         
-        // Firebase Log In
+        // Firebase Register
+        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: {authResult, error in
+            guard let result = authResult, error == nil else {
+                print("Error creating user")
+                return
+            }
+            
+            let user = result.user
+            print("Created User: \(user)")
+        })
     }
     
     func alertUserLoginError() {
